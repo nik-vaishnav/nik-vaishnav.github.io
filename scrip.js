008d08e68d9a38c800f594da5acc7aa206ff3a61
nik-vaishnav <nikhilv121121@gmail.com>
2024-09-28 01:03:36 +0530

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,5 @@
+// This script will alert a message when the button is clicked
+
+document.getElementById('portfolioBtn').addEventListener('click', function() {
+    alert('Welcome to Nikhil Vaishnav\'s Portfolio!');
+});
